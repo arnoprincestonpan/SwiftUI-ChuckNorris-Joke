7fbbf71f07b9b87605d1e232d7feec45d3c29607
Arno Pan <apan11@my.bcit.ca>
2023-07-10 16:11:41 -0700

    Initial Commit

---
+++ JokeFetchApi/JokeFetchApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFetchApiApp.swift
+//  JokeFetchApi
+//
+//  Created by Arno Pan on 2023-07-10.
+//
+
+import SwiftUI
+
+@main
+struct JokeFetchApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
